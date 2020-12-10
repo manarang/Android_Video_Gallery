abde5a298cabf25c0b133cd6b5cb1e13d5d01a90
Petrus Jonatan <64063361+manarang@users.noreply.github.com>
2020-12-10 20:49:15 +0700

    Update RequestHandler.java

--- java/RequestHandler.java
+++ java/RequestHandler.java
@@ -1,1 +1,68 @@
+package com.vg;
+
+import android.app.AlertDialog;
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class RequestHandler {
+
+    public String sendGetRequest(String requestURL, Context context){
+
+        StringBuilder sb = new StringBuilder();
+        try {
+            URL url = new URL(requestURL);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+            String s;
+            while ((s = bufferedReader.readLine()) != null){
+                sb.append(s+"\n");
+            }
+
+        } catch (MalformedURLException e) {
+            new AlertDialog.Builder(context)
+                    .setMessage(e.getMessage())
+                    .show();
+        } catch (IOException e) {
+            new AlertDialog.Builder(context)
+                    .setMessage(e.getMessage())
+                    .show();
+        }catch (NullPointerException e){
+            new AlertDialog.Builder(context)
+                    .setMessage(e.getMessage())
+                    .show();
+        }
+        return sb.toString();
+
+    }
+
+    public String sendGetRequestParam(String requestURL,String id){
+
+        StringBuilder sb = new StringBuilder();
+        try {
+            URL url = new URL(requestURL+id);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+            String s;
+            while ((s = bufferedReader.readLine()) != null){
+                sb.append(s+"\n");
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+
+    }
+
+}
 
